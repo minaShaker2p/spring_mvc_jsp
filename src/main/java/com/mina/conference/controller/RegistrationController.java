dba073038c1ff9764be9684a94614d8c8d76ca6e
MinaRezkalla <mina.shaker@appsfactory.de>
2020-06-16 17:21:21 +0200

    add validation

--- src/main/java/com/mina/conference/controller/RegistrationController.java
+++ src/main/java/com/mina/conference/controller/RegistrationController.java
@@ -2,22 +2,30 @@ package com.mina.conference.controller;
 
 import com.mina.conference.model.Registration;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/conference/")
 public class RegistrationController {
 
     @GetMapping("registration")
-    public String getRegistration(@ModelAttribute ("registration")Registration registration) {
+    public String getRegistration(@ModelAttribute("registration") Registration registration) {
         return "registration";
     }
 
     @PostMapping("registration")
-    public String addRegistration(@ModelAttribute ("registration")Registration registration) {
+    public String addRegistration(@Valid @ModelAttribute("registration") Registration registration,
+                                  BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            System.out.println("There were some errors here");
+            return "registration";
+        }
         System.out.println("Registration: " + registration.getName());
         return "redirect:registration";
     }
