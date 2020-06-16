9f03749146727cdf49f1e69cf3a719f150ccdc97
MinaRezkalla <mina.shaker@appsfactory.de>
2020-06-16 14:13:47 +0200

    add view resolver and static resolver

---
+++ src/main/java/com/mina/conference/controller/RegistrationController.java
@@ -0,0 +1,25 @@
+package com.mina.conference.controller;
+
+import com.mina.conference.model.Registration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/conference/")
+public class RegistrationController {
+
+    @GetMapping("registration")
+    public String getRegistration(@ModelAttribute ("registration")Registration registration) {
+        return "registration";
+    }
+
+    @PostMapping("registration")
+    public String addRegistration(@ModelAttribute ("registration")Registration registration) {
+        System.out.println("Registration: " + registration.getName());
+        return "registration";
+    }
+
+}
